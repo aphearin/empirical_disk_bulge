461546b723633d6491426ce336861869654df360
Andrew Hearin <andrew.hearin@yale.edu>
2017-05-29 22:21:31 -0700

    Starting to develop disk_bulge_simple_disruption function

--- empirical_disk_bulge/disk_bulge_simple_disruption.py
+++ empirical_disk_bulge/disk_bulge_simple_disruption.py
@@ -8,5 +8,27 @@ from .engines import disk_bulge_simple_disruption_engine as disk_bulge_engine
 __all__ = ('disk_bulge_simple_disruption', )
 
 
-def disk_bulge_simple_disruption():
-    pass
+def disk_bulge_simple_disruption(sfr_history, sm_history,
+        disruption_prob_history, cosmic_age_array, redshift_obs, frac_migration_array):
+    """
+    """
+    merger_history = calculate_merger_history(sfr_history, sm_history)
+    disruption_prob_history = calculate_disruption_prob_history()
+
+    sm_decomposition = np.array(disk_bulge_engine(
+        sfr_history, merger_history, disruption_prob_history,
+        frac_migration_array, cosmic_age_array, redshift_obs))
+    disk, bulge = sm_decomposition[:, 0], sm_decomposition[:, 1]
+    return disk, bulge
+
+
+def calculate_merger_history(sfr_history, sm_history, cosmic_age_array):
+    """
+    """
+    raise NotImplementedError("Not implemented yet")
+
+
+def calculate_disruption_prob_history(*args):
+    """
+    """
+    raise NotImplementedError("Not implemented yet")
--- empirical_disk_bulge/engines/disk_bulge_simple_disruption_engine.pyx
+++ empirical_disk_bulge/engines/disk_bulge_simple_disruption_engine.pyx
@@ -20,7 +20,7 @@ cdef double random_uniform():
 @cython.nonecheck(False)
 def disk_bulge_simple_disruption_engine(double[:, :] in_situ_sfr_history,
         double[:, :] merging_history, double[:, :] disruption_prob_history,
-        double frac_migration, cosmic_age_array, redshift_obs):
+        double[:] frac_migration_array, cosmic_age_array, redshift_obs):
     """
     """
     cdef int idx_zobs = _index_of_nearest_larger_redshift(redshift_obs)
@@ -38,6 +38,7 @@ def disk_bulge_simple_disruption_engine(double[:, :] in_situ_sfr_history,
     cdef int igal, itime
     cdef double sm_bulge, sm_disk, merging_dsm, disruption_prob
     cdef double disk_to_bulge_migration_mass, in_situ_dsm
+    cdef double frac_migration
 
     #  Outer loop is over rows, one for each galaxy in the mock
     for igal in range(num_gals):
@@ -63,6 +64,7 @@ def disk_bulge_simple_disruption_engine(double[:, :] in_situ_sfr_history,
 
             #  Disrupt the disk according to the input probability
             disruption_prob = disruption_prob_history[igal, itime]
+            frac_migration = frac_migration_array[itime]
             if random_uniform() < disruption_prob:
                 disk_to_bulge_migration_mass = frac_migration*sm_disk
                 sm_bulge += disk_to_bulge_migration_mass
