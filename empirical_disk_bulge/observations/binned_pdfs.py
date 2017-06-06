f213b1230d21afcdcfd230acccb03df27593efaa
Andrew Hearin <andrew.hearin@yale.edu>
2017-06-06 16:12:24 -0700

    Added new bt_pdf_fixed_sm_ssfr function and notebook providing sketch of usage

--- empirical_disk_bulge/observations/binned_pdfs.py
+++ empirical_disk_bulge/observations/binned_pdfs.py
@@ -22,9 +22,9 @@ def gaussian_kernel_selection(x, mu, sigma):
 
     Returns
     -------
-    prob : float or ndarray
-        Float or ndarray of shape (ngals, ) storing
-        the probability that the input galaxy will be selected by the filtering kernel
+    mask : float or ndarray
+        Boolean array of shape (ngals, ) that can be used as a mask to select
+        values of the input ``x`` passing the kernel selection
 
     Examples
     ---------
@@ -34,4 +34,23 @@ def gaussian_kernel_selection(x, mu, sigma):
     >>> selection_prob = gaussian_kernel_selection(stellar_mass, mu, sigma)
     >>> selection_mask = np.random.rand(ngals) < selection_prob
     """
-    return 1 - 2*np.abs(0.5 - norm.cdf(x, loc=mu, scale=sigma))
+    prob = 1 - 2*np.abs(0.5 - norm.cdf(x, loc=mu, scale=sigma))
+    return np.random.rand(len(x)) < prob
+
+
+def bt_pdf_fixed_sm(bt, sm, fixed_sm, bt_bins, sigma=0.1):
+    """
+    """
+    sample_mask = gaussian_kernel_selection(sm, fixed_sm, sigma=sigma)
+    assert np.count_nonzero(sample_mask) > 100, "Must have at least 100 galaxies in bin to estimate PDF"
+    return np.histogram(bt[sample_mask], bt_bins, density=True)[0]
+
+
+def bt_pdf_fixed_sm_ssfr(bt, sm, ssfr, fixed_sm, fixed_ssfr, bt_bins,
+        sigma_sm=0.1, sigma_ssfr=0.25):
+    """
+    """
+    sm_sample_mask = gaussian_kernel_selection(sm, fixed_sm, sigma=sigma_sm)
+    ssfr_mask = gaussian_kernel_selection(ssfr[sm_sample_mask], fixed_ssfr, sigma=sigma_ssfr)
+    assert np.count_nonzero(ssfr_mask) > 100, "Must have at least 100 galaxies in bin to estimate PDF"
+    return np.histogram(bt[sm_sample_mask][ssfr_mask], bt_bins, density=True)[0]
